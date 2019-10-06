89124e2419821ae06f24f25afc754a279af8bca9
grilohbg <henriqueborgesgarcia@gmail.com>
2019-10-06 10:00:52 -0300

    jacobiseq prints executable bad usage error

--- jacobiseq/jacobiseq.c
+++ jacobiseq/jacobiseq.c
@@ -4,9 +4,24 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
-#pragma omp parallel
-    printf("oii\n");
+    char help_message[300];
+    sprintf(help_message,
+            "Usage:\n"
+            "\t%s <N> <T>\n\n"
+            "Where:\n"
+            "\tN is the amount of equations pseudorandomly generated\n"
+            "\tT is the amount of POSIX threads that will be used\n",
+            argv[0]);
+
+    if( argc != 3)
+    {
+        printf("%s", help_message);
+        exit(0);
+    }
+
+    printf("N = %s\nT = %s", argv[1], argv[2]);
 }
